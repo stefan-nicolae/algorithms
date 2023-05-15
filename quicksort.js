606ae19e9bcb70514eac10bd9b97c53e781b3b35
stefan-nicolae <84786481+stefan-nicolae@users.noreply.github.com>
2023-05-15 16:46:39 +0300

    Create quicksort.js

---
+++ quicksort.js
@@ -0,0 +1,22 @@
+function swap(a, i, j) {
+    const t = a[j]
+    a[j] = a[i]
+    a[i] = t
+}
+
+function quicksort (a) {
+    if(a.length < 1) return a
+    let pivot = a[a.length - 1]
+    let i = 0, j = 0; 
+    while(j < a.length - 1) {
+        if(a[j] < pivot) swap(a, i++, j)
+        j++;
+    }
+    swap(a, i, a.length - 1) 
+    pivot = a[i]
+    const left = quicksort(a.slice(0, i))
+    const right = quicksort(a.slice(i + 1 , a.length))
+    return left.concat([pivot].concat(right))
+}   
+
+quicksort([8, 2, 4, 7, 1, 3, 9, 6, 5])
